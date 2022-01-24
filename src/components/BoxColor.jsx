36c211cf3886b6bae27fefa873c964c3cecd172c
Anna Baldwin <annabaldwin56@gmail.com>
2022-01-24 22:20:17 +0100

    more lab changes

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BoxColor = ({r, g, b}) => {
-  return <div style={{backgroundColor: `rgb(${r},${g},${b})`}}>
+  return <div class='boxcolor' style={{backgroundColor: `rgb(${r},${g},${b})`}}>
       rgb({r},{g},{b})
   </div>
 }
